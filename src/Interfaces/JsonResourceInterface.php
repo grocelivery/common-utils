2afe45050ce4fbeee88cefdefbb1a1eeda1253bd
Mateusz Lencki <mateusz.lencki@gmail.com>
2019-11-12 21:09:43 +0100

    Removed composer.lock

--- src/Interfaces/JsonResourceInterface.php
+++ src/Interfaces/JsonResourceInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Grocelivery\CommonUtils\Interfaces;
 
-use Illuminate\Support\Collection;
-
 /**
  * Interface JsonResourceInterface
  * @package Grocelivery\CommonUtils\Interfaces
